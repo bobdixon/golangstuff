bf4ebaa3248be030d666ace47ae0f3c036a6766b
Bob Dixon <chadwalker@gmail.com>
2017-09-06 08:57:12 -0500

    commit time

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import "fmt"
-import "os"
-
 func main() {
 
-	var s string
-	var sep string
-	for i := 1; i < len(os.Args); i++ {
-
-		s += sep + os.Args[i]
-		sep = " "
-	}
-	fmt.Println(s)
 }
